63168615d6282d74dbfd1ef588f3be63bc0e3059
Anmol Singh <asingh@quora.com>
2023-08-05 18:15:04 -0700

    handle system role

--- nous_hermes_13b.py
+++ nous_hermes_13b.py
@@ -37,6 +37,8 @@ class NousHermes13BBot(PoeBot):
                 prompt += f"### Input: {message.content}\n\n"
             elif message.role == "bot":
                 prompt += f"### Response: {message.content}\n\n"
+            elif message.role == "system":
+                pass
             else:
                 raise ValueError(f"unknown role {message.role}.")
         prompt += "### Response:"
